11fd676eea87036bf3947a582517e54dca91214f
svv0003 <ssetoo4689@naver.com>
2025-12-18 17:42:50 +0900

    12/18 day3

--- front-end/src/service/apiService.js
+++ front-end/src/service/apiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_BASE_URL = '/api';
+const API_BASE_URL = '/api';
 // export const API_BASE_URL = 'http://localhost:9000/api';
 
 const api = axios.create({
